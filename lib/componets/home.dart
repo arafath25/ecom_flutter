bd40e8a75cbab6fd511de04c5ddb1a00e9a584fe
arafath25 <arafatctg58@yahoo.com>
2019-12-18 02:23:07 +0600

    flutter ecommerce frist commit

---
+++ lib/componets/home.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter_ecom/componets/horizontal_listview.dart';
+import 'package:flutter_ecom/componets/products.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget image_carousel = new Container(
+      height: 200.0,
+      child: new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/m2.jpg'),
+          AssetImage('images/w1.jpeg'),
+          AssetImage('images/w3.jpeg'),
+          AssetImage('images/w4.jpeg'),
+        ],
+        autoplay: false,
+        /*animationCurve: Curves.fastOutSlowIn,
+      animationDuration: Duration(microseconds: 1000),*/
+        dotSize: 4.0,
+        indicatorBgPadding: 2.0,
+      ),
+    );
+
+    return Scaffold(
+      appBar: new AppBar(
+        elevation: 0.1,
+        backgroundColor: Colors.red,
+        title: Text('FashApp'),
+        actions: <Widget>[
+          new IconButton(icon: Icon(Icons.search, color: Colors.white,), onPressed: (){}),
+          new IconButton(icon: Icon(Icons.shopping_cart, color: Colors.white,), onPressed: (){})
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+//            header
+            new UserAccountsDrawerHeader(
+              accountName: Text('arafat hossain'),
+              accountEmail: Text('arafatcse14@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.person, color: Colors.white,),
+                ),
+              ),
+              decoration: new BoxDecoration(
+                  color: Colors.red
+              ),
+            ),
+
+//            body
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My account'),
+                leading: Icon(Icons.person),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My Orders'),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Categoris'),
+                leading: Icon(Icons.dashboard),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Favourites'),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings),
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+
+      body: new ListView(
+        children: <Widget>[
+//          image carousel begins here
+          image_carousel,
+          new Padding(padding: const EdgeInsets.all(8.0),
+            child: new Text('Category'),),
+
+//          Horizontal list view begins here
+          HorizontalList(),
+          new Padding(padding: const EdgeInsets.all(20.0),
+            child: new Text('Recent products'),),
+
+          Container(
+            height: 320.0,
+            child: Products(),
+          )
+        ],
+      ),
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecom/componets/home.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    )
+  );
+}
